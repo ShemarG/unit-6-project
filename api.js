357a0df74c2bdb2afa8afb156724d91acad264c9
segunsub <segunsubair3@gmail.com>
2021-03-03 14:34:25 -0500

    images present

--- api.js
+++ api.js
@@ -1,16 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-   document.querySelector('button').addEventListener('click',() => {  apicall().then(data => console.log(data))})
+   document.getElementById('searchbar').addEventListener('submit',(e) => { 
+       e.preventDefault()
+        apicall(e.target[0].value).then(data => process(data.results))
+})
+let home = document.getElementById('home')
 })
 // function fetch() {
 //     apicall()
 //     .then(data => console.log(data))
 // }
-// function process(obj) {
-//     console.log(obj)
-// }
+function process(obj) {
+    console.log(obj)
+    obj.forEach(multi => {
+        let imgdiv = document.createElement('div')
+        if(multi.poster_path === null){
+            imgdiv.innerHTML = `<img src="img/noimg.jpg"/>`
+        }else {
+            imgdiv.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${multi.poster_path}"/>`
+        }
+        console.log(multi.poster_path)
+       
+        home.append(imgdiv)
+    })
+  
+}
 
-async function apicall(){
-   let res = await fetch(`http://api.tvmaze.com/singlesearch/shows?q=avengers`)
+async function apicall(input){
+   let res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${Api.key}&language=en-US&query=${input}`)
     let data = await res.json()
     return data
 }
